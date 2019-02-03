80a504b041908eb133a27065b69dec963f2c4373
Nobuyuki Umetani <n.umetani@gmail.com>
2019-02-03 16:14:23 +0900

    hoge

--- util_network.py
+++ util_network.py
@@ -309,6 +309,39 @@ class NetDiscriminator256(torch.nn.Module):
       my_torch.ModuleConv_k4_s2(256, 512, is_leaky=True), # 1/16
       my_torch.ModuleConv_k4_s2(512, 512, is_leaky=True),  # 1/32
       my_torch.ModuleConv_k4_s2(512, 512, is_leaky=True),  # 1/64
+    )
+    self.layer1 = torch.nn.Sequential(
+      torch.nn.Linear(512*4*4,1),
+      torch.nn.Sigmoid()
+    )
+    my_torch.initialize_net(self)
+    ####
+    if os.path.isfile(path_file):
+      self = my_torch.load_model_cpm(self, path_file)
+    ####
+    if torch.cuda.is_available():
+      self = self.cuda()
+
+  def forward(self, x0):
+    assert len(x0.shape) == 4
+    assert list(x0.shape[1:]) == [3,256,256]
+    x1 = self.layer0(x0)
+    x1 = x1.view((x0.shape[0],-1))
+    x2 = self.layer1(x1)
+    return x2
+
+class NetDiscriminator256_A(torch.nn.Module):
+  def __init__(self, path_file: str):
+    super(NetDiscriminator256_A, self).__init__()
+    self.path_file = path_file
+    self.nstride = 32
+    self.layer0 = torch.nn.Sequential(
+      torch.nn.Conv2d(3, 64, kernel_size=4, padding=1, stride=2), # 1/2
+      my_torch.ResUnit_BRC_ResHalf_Cat_B( 64, is_leaky=True, is_separable=True),  # 1/4
+      my_torch.ResUnit_BRC_ResHalf_Cat_B(128, is_leaky=True, is_separable=True),  # 1/8
+      my_torch.ResUnit_BRC_ResHalf_Cat_B(256, is_leaky=True, is_separable=True),  # 1/16
+      my_torch.ResUnit_BRC_ResHalf_Add_C(512, is_leaky=True, is_separable=True),  # 1/32
+      my_torch.ResUnit_BRC_ResHalf_Add_C(512, is_leaky=True, is_separable=True),  # 1/64
       torch.nn.BatchNorm2d(512),
       torch.nn.LeakyReLU(inplace=True,negative_slope=0.2)
     )
--- util_torch.py
+++ util_torch.py
@@ -15,7 +15,6 @@ def np2pt(np_img,scale,requires_grad=False):
     vpt_img = vpt_img.cuda()
   return vpt_img
 
-
 def load_model_cpm(net_cpm,path):
   if os.path.isfile(path):
     print("load model from",path)
@@ -205,6 +204,80 @@ class ResUnit_BRC_ResHalf_Cat(torch.nn.Module):
     y = torch.nn.functional.max_pool2d(self.conv1(x),kernel_size=4,padding=1,stride=2)
     return torch.cat((y,self.model(x)),1)
 
+
+class ResUnit_BRC_ResHalf_Cat_A(torch.nn.Module):
+  def __init__(self, nc_in, nc_out, is_separable=False, is_leaky=True, bn=True):
+    super(ResUnit_BRC_ResHalf_Cat_A, self).__init__()
+    nh = nc_out//2
+    assert nc_out == nh*2
+    ngroup = nh if is_separable else 1
+    ####
+    layers = []
+    if bn == True:
+      layers.append( torch.nn.BatchNorm2d(nc_in) )
+    if is_leaky:
+      layers.append( torch.nn.LeakyReLU(inplace=True,negative_slope=0.2) )
+    else:
+      layers.append( torch.nn.ReLU(inplace=True) )
+    layers.append( torch.nn.Conv2d(nc_in, nh, kernel_size=4, padding=1, stride=2) )
+    self.layer0 = nn.Sequential(*layers)
+    ####
+    layers = []
+    layers.append( torch.nn.BatchNorm2d(nh) )
+    if is_leaky:
+      layers.append( torch.nn.LeakyReLU(inplace=True,negative_slope=0.2) )
+    else:
+      layers.append( torch.nn.ReLU(inplace=True) )
+    layers.append( torch.nn.Conv2d(nh, nh, kernel_size=3, padding=1, stride=1, groups=ngroup) )
+    self.layer1 = nn.Sequential(*layers)
+    initialize_net(self)
+
+  def forward(self, x):
+    x = self.layer0(x)
+    y = self.layer1(x)
+    return torch.cat((y,x),1)
+
+class ResUnit_BRC_ResHalf_Cat_B(torch.nn.Module):
+  def __init__(self, nc, is_leaky=True, is_separable=True):
+    super(ResUnit_BRC_ResHalf_Cat_B, self).__init__()
+    ngroup = nc if is_separable else 1
+    ####
+    layers = []
+    layers.append( torch.nn.Conv2d(nc, nc, kernel_size=3, padding=1, stride=1) )
+    layers.append( torch.nn.BatchNorm2d(nc) )
+    if is_leaky:
+      layers.append( torch.nn.LeakyReLU(inplace=True,negative_slope=0.2) )
+    else:
+      layers.append( torch.nn.ReLU(inplace=True) )
+    layers.append( torch.nn.Conv2d(nc, nc, kernel_size=4, padding=1, stride=2, groups=ngroup) )
+    self.layer0 = nn.Sequential(*layers)
+    initialize_net(self)
+
+  def forward(self, x0):
+    x1 = torch.nn.functional.avg_pool2d(x0,kernel_size=2,stride=2)
+    y = self.layer0(x0)
+    return torch.cat((y,x1),1)
+
+class ResUnit_BRC_ResHalf_Add_C(torch.nn.Module):
+  def __init__(self, nc, is_separable=False, is_leaky=True):
+    super(ResUnit_BRC_ResHalf_Add_C, self).__init__()
+    ngroup = nc if is_separable else 1
+    ####
+    layers = []
+    layers.append( torch.nn.Conv2d(nc, nc, kernel_size=3, padding=1, stride=1) )
+    layers.append( torch.nn.BatchNorm2d(nc) )
+    if is_leaky:
+      layers.append( torch.nn.LeakyReLU(inplace=True,negative_slope=0.2) )
+    else:
+      layers.append( torch.nn.ReLU(inplace=True) )
+    layers.append( torch.nn.Conv2d(nc, nc, kernel_size=4, padding=1, stride=2, groups=ngroup) )
+    self.layer0 = nn.Sequential(*layers)
+    initialize_net(self)
+
+  def forward(self, x0):
+    x1 = torch.nn.functional.avg_pool2d(x0,kernel_size=2,stride=2)
+    y = self.layer0(x0)
+    return y+x1
 
 class ResUnit_BRC_ResDouble(torch.nn.Module):
   def __init__(self, nc_in,nc_out,is_separable=False):
