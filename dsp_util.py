e98d2b26cb2a9e98f37a67dc6bf3b03e5077d4c6
Nobuyuki Umetani <n.umetani@gmail.com>
2019-02-04 17:22:34 +0900

    get_img_kp

--- dsp_util.py
+++ dsp_util.py
@@ -52,11 +52,9 @@ def get_affine(dict_info, size_input, size_output, rot_range=(-40,40), mag_range
   rot_mat[1][2] += size_output[1] * 0.5 - cnt[1] + random.randint(int(-8 / scale), int(+8 / scale))
   return rot_mat, scale
 
-def get_img_kp(list_name_kp,dict_info, size_img_out,rot_mat,nstride,dist_size):
+def get_img_kp(list_name_kp,dict_info, size_img_out,rot_mat,dist_size):
   nch_out = len(list_name_kp)
-  nblk_h = size_img_out[0]//nstride
-  nblk_w = size_img_out[1]//nstride
-  np_wht0 = numpy.zeros((1,nblk_h,nblk_w, nch_out), dtype=numpy.float32)
+  np_wht0 = numpy.zeros((1,size_img_out[0],size_img_out[1], nch_out), dtype=numpy.float32)
   dict_prsn = dict_info["person0"]
   for ikey, key in enumerate(list_name_kp):
     if not key in dict_prsn: continue
@@ -65,9 +63,7 @@ def get_img_kp(list_name_kp,dict_info, size_img_out,rot_mat,nstride,dist_size):
     dx0 = rot_mat[0][0] * cx0 + rot_mat[0][1] * cy0 + rot_mat[0][2]
     dy0 = rot_mat[1][0] * cx0 + rot_mat[1][1] * cy0 + rot_mat[1][2]
     my_util.gauss_keypoint(np_wht0[0], ikey,
-                           dx0 / nstride,
-                           dy0 / nstride,
-                           dist_size / nstride)
+                           dx0, dy0, dist_size)
   return np_wht0
 
 def get_img_seg(list_name_seg, dict_info, size_img_in, size_img_out, rot_mat):
