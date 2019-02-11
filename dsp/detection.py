0296e26758bb478f2a5ec47694bb0550a8328cec
Nobuyuki Umetani <n.umetani@gmail.com>
2019-02-11 21:46:54 +0900

    hoge

--- dsp/detection.py
+++ dsp/detection.py
@@ -1,8 +1,9 @@
-import os, cv2, math
+import os, cv2, math, numpy, json, glob, random
 import torch
 import torch.nn.functional
 import my_dnn_util.util_torch as my_torch
 import my_dnn_util.util as my_util
+import my_dnn_util.dsp.util as my_dsp
 
 class NetDetect_s32(torch.nn.Module):
   def __init__(self,path_file):
@@ -184,6 +185,67 @@ class NetDetect_s16_B(torch.nn.Module):
     out_prm = self.prm(out_dsc)
     return out_cls, out_prm
 
+####################################
+
+def affine_random_head(dict_info,size_input, size_output, target_rad):
+  dict_prsn = dict_info["person0"]
+  scale = target_rad / dict_prsn["rad_head"]
+  scale *= random.uniform(0.5, 2.0)
+  if 'kp_head' in dict_prsn:
+    x0 = dict_prsn["kp_head"][0]
+    y0 = dict_prsn["kp_head"][1]
+    cnt = [x0+random.randint(-64,64), y0+random.randint(-64,64)] # x,y
+  else:
+    cnt = [size_input[0] * 0.5, + size_input[1] * 0.5]
+  rot = random.randint(-40, 40)
+  rot_mat = cv2.getRotationMatrix2D(tuple([cnt[0], cnt[1]]), rot, scale)
+  rot_mat[0][2] += size_output[0] * 0.5 - cnt[0] + random.randint(int(-8 / scale), int(+8 / scale))
+  rot_mat[1][2] += size_output[1] * 0.5 - cnt[1] + random.randint(int(-8 / scale), int(+8 / scale))
+  return rot_mat, scale
+
+class BatchesHead:
+  def __init__(self, list_path_dir_img:list, nbatch):
+    list_path_json = []
+    for path_dir_img in list_path_dir_img:
+      list_path_json += glob.glob(path_dir_img + "/*.json")
+    self.bd = my_util.BatchDispenser(nbatch, list_path_json)
+
+  def get_batch(self,nstride):
+    size_img = 32*8
+    nblk = size_img // nstride
+    np_batch_in = numpy.empty((0, size_img, size_img, 3), dtype=numpy.uint8)
+    np_batch_tgc = numpy.empty((0, nblk, nblk), dtype=numpy.int64)
+    np_batch_tgl = numpy.empty((0, 3, nblk, nblk), dtype=numpy.float32)
+    np_batch_msk = numpy.empty((0, nblk, nblk), dtype=numpy.float32)
+    ####
+    for path_json in self.bd.get_batch_path():
+      dict_info = json.load(open(path_json, "r"))
+      np_img0 = cv2.imread(my_util.path_img_same_name(path_json))
+      rot_mat, scale = affine_random_head(dict_info,
+                                          (np_img0.shape[1], np_img0.shape[0]), (size_img, size_img),
+                                          target_rad=nstride*0.72)
+      np_img1 = cv2.warpAffine(np_img0, rot_mat, (size_img, size_img), flags=cv2.INTER_CUBIC)
+      np_tgc, np_tgl, np_msk = my_dsp.input_detect("kp_head",dict_info,nblk,nstride,rot_mat,scale)
+      ####
+      np_batch_in = numpy.vstack((np_batch_in, np_img1.reshape(1, size_img, size_img, 3)))
+      np_batch_tgc = numpy.vstack((np_batch_tgc, np_tgc))
+      np_batch_tgl = numpy.vstack((np_batch_tgl, np_tgl))
+      np_batch_msk = numpy.vstack((np_batch_msk, np_msk))
+    np_batch_in = np_batch_in.astype(numpy.uint8)
+    return np_batch_in,np_batch_tgc,np_batch_tgl,np_batch_msk
+
+  def get_batch_vpt(self,nstride):
+    np_in, np_tgc, np_tgl, np_msk = self.get_batch(nstride)
+    vpt_in = my_torch.np2pt_img(np_in,scale=2.0/255.0,offset=-1.0,requires_grad=True)
+    vpt_tgc = torch.autograd.Variable(torch.from_numpy(np_tgc), requires_grad=False)
+    vpt_tgl = torch.autograd.Variable(torch.from_numpy(np_tgl), requires_grad=False)
+    vpt_msk = torch.autograd.Variable(torch.from_numpy(np_msk), requires_grad=False)
+    if torch.cuda.is_available():
+      vpt_tgc = vpt_tgc.cuda()
+      vpt_tgl = vpt_tgl.cuda()
+      vpt_msk = vpt_msk.cuda()
+    return vpt_in,vpt_tgc,vpt_tgl,vpt_msk
+
 
 ##################################
 
@@ -208,6 +270,30 @@ def detection_view_out(np_out,np_cls,np_prm,nstride):
   cv2.imshow("hoge", np_out)
   cv2.waitKey(-1)
 
+def view_in_tg(np_in,np_tgc,np_tgl,np_msk,nstride):
+  nblkh = np_in.shape[0]//nstride
+  nblkw = np_in.shape[1]//nstride
+  assert np_tgc.shape == (nblkh,nblkw)
+  assert np_tgl.shape == (3,nblkh,nblkw)
+  np_out = np_in.copy()
+  for iblkh in range(nblkh):
+    for iblkw in range(nblkw):
+      if np_msk[iblkh][iblkw] == 1:
+        my_util.cv2_draw_circle(np_out,
+                                ((iblkw + 0.5) * nstride, (iblkh + 0.5) * nstride),
+                                color=(0, 255, 0), rad=nstride*0.72)
+      if np_tgc[iblkh][iblkw] != 1:
+        continue
+      p0x = iblkw*nstride+nstride//2
+      p0y = iblkh*nstride+nstride//2
+      print(np_tgl[0][iblkh][iblkw],np_tgl[1][iblkh][iblkw],np_tgl[2][iblkh][iblkw])
+      p1x = np_tgl[0][iblkh][iblkw]*2.0*nstride + p0x
+      p1y = np_tgl[1][iblkh][iblkw]*2.0*nstride + p0y
+      rs = math.pow(2,np_tgl[2][iblkh][iblkw]*2.0)*nstride
+      my_util.cv2_draw_circle(np_out,(p1x,p1y),rs,color=(0,0,255))
+      my_util.cv2_draw_line(np_out,(p1x,p1y),(p0x,p0y),color=(0,255,0))
+  cv2.imshow("hoge", np_out)
+  cv2.waitKey(-1)
 
 def detect(img_in,net_detect,mag:int,prob_thre):
   nstride = net_detect.nstride
