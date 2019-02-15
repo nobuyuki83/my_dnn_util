4ecc9da5db35ec159836e82f1c7c926cd1a5b89c
Nobuyuki Umetani <n.umetani@gmail.com>
2019-02-15 11:14:20 +0900

    hoge

--- __init__.py
+++ __init__.py
@@ -2,3 +2,4 @@ from my_dnn_util.util import *
 from my_dnn_util.util_torch import *
 from my_dnn_util.net_encdec import *
 from my_dnn_util.util_network import *
+from my_dnn_util.net_classify import *
--- dsp/util_gan.py
+++ dsp/util_gan.py
@@ -71,3 +71,80 @@ class Batches_MaskSeg_InKp:
     if torch.cuda.is_available():
       vpt_tg_fake = vpt_tg_fake.cuda()
     return vpt_out_real,vpt_out_fake,vpt_tg_fake
+
+
+
+#####################################################################################
+class Batches_MaskKp_InSegKp:
+  def __init__(self,path_dir_img:str, nbatch:int, list_name_kp_mask:list, list_name_kp_pose):
+    list_path_json = glob.glob(path_dir_img + "/*.json")
+    list_path_json = my_dsp.list_annotated_or(list_path_json,list_name_kp_mask)
+    list_path_json = my_dsp.list_annotated_or(list_path_json,["seg"])
+    self.bd = my_util.BatchDispenser(nbatch, list_path_json)
+    self.list_name_kp_mask = list_name_kp_mask
+    self.list_name_kp_pose = list_name_kp_pose
+    print("number of images:", len(self.bd.list_path_in))
+
+
+  def get_batch_np(self, nstride:int):
+    size_img = 224
+    nblk = size_img // nstride
+    np_batch_bgr0 = numpy.empty((0,size_img,size_img,3),dtype=numpy.uint8)
+    np_batch_mask0 = numpy.empty((0, nblk, nblk, 1), dtype=numpy.float32)
+    np_batch_seg0 = numpy.empty((0, nblk, nblk, 1), dtype=numpy.float32)
+    np_batch_kp0 = numpy.empty((0, nblk, nblk, len(self.list_name_kp_pose)), dtype=numpy.float32)
+    ####
+    for path_json in self.bd.get_batch_path():
+      dict_info = json.load(open(path_json,"r"))
+      np_img0 = cv2.imread(my_util.path_img_same_name(path_json))
+      np_img0 = my_util.pca_color_augmentation(np_img0)
+      rot_mat,scale = my_dsp.get_affine(dict_info,
+                                        (np_img0.shape[1], np_img0.shape[0]),
+                                        (size_img, size_img),
+                                        npix_face_rad=24)
+      ####
+      np_img1 = cv2.warpAffine(np_img0, rot_mat, (size_img,size_img), flags=cv2.INTER_CUBIC)
+      assert np_img1.shape == (nblk * nstride, nblk * nstride, 3)
+      ####
+      np_mask = my_dsp.input_kp_assembled(self.list_name_kp_mask, dict_info,
+                                          (size_img, size_img), rot_mat, np_img0.shape,
+                                          16/scale)
+      np_seg = my_dsp.input_seg(["seg"],dict_info,
+                                (size_img, size_img), rot_mat, np_img0.shape)
+      np_kp = my_dsp.input_kp(self.list_name_kp_pose, dict_info, (size_img, size_img), rot_mat,
+                              dict_info["person0"]["rad_head"]*scale*0.5)
+      ####
+      np_batch_bgr0 = numpy.vstack((np_batch_bgr0,np_img1.reshape(1,size_img,size_img,3)))
+      np_batch_mask0 = numpy.vstack((np_batch_mask0, np_mask))
+      np_batch_seg0 = numpy.vstack((np_batch_seg0, np_seg))
+      np_batch_kp0 = numpy.vstack((np_batch_kp0, np_kp))
+    np_batch_bgr0 = np_batch_bgr0.astype(numpy.uint8)
+    return np_batch_bgr0, np_batch_mask0, np_batch_seg0, np_batch_kp0
+
+  def get_batch_vpt_real(self,nstride):
+    np_batch_bgr, np_batch_mask, np_batch_seg, np_batch_kp = self.get_batch_np(nstride)
+    vpt_batch_bgr = my_torch.np2pt_img(np_batch_bgr,2.0/255.0,offset=-1.0,requires_grad=False)
+    vpt_batch_mask = my_torch.np2pt_img(np_batch_mask,1.0, offset=0.0,requires_grad=False)
+    vpt_batch_seg = my_torch.np2pt_img(np_batch_seg,1.0,offset=0.0,requires_grad=False)
+    vpt_batch_kp = my_torch.np2pt_img(np_batch_kp,1.0,offset=0.0,requires_grad=False)
+    return vpt_batch_bgr, vpt_batch_mask, vpt_batch_seg, vpt_batch_kp
+
+  def get_batch_vpt_realfake(self,net_gen, requires_grad_fake=False):
+    vpt_bgr, vpt_mask, vpt_seg, vpt_kp  = self.get_batch_vpt_real(net_gen.nstride)
+    vpt_bgr1 = vpt_bgr * (1 - vpt_mask)
+    vpt_in = torch.cat((vpt_bgr1, vpt_mask, vpt_seg, vpt_kp), 1)
+    if requires_grad_fake:
+      vpt_in.requires_grad = True
+      vpt_out0 = net_gen.forward(vpt_in)
+    else:
+      with torch.no_grad():
+        vpt_out0 = net_gen.forward(vpt_in)
+      vpt_out0 = vpt_out0.detach()
+    vpt_bgr_fake = vpt_out0 * vpt_mask + vpt_bgr * (1 - vpt_mask)
+    ####
+    vpt_out_real = torch.cat((vpt_bgr,vpt_seg,vpt_kp),dim=1)
+    vpt_out_fake = torch.cat((vpt_bgr_fake,vpt_seg,vpt_kp),dim=1)
+    vpt_tg_fake = my_torch.get_mask_ratio_vpt(vpt_mask)
+    if torch.cuda.is_available():
+      vpt_tg_fake = vpt_tg_fake.cuda()
+    return vpt_out_real,vpt_out_fake,vpt_tg_fake
